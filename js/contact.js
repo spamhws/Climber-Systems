0fefa591c7948ad8f96d3e36191b7875397a0aaf
spamhws <egorsanko@gmail.com>
2022-03-13 22:19:10 +0100

    early works on contact page

---
+++ js/contact.js
@@ -0,0 +1,18 @@
+function reveal() {
+  let reveals = document.querySelectorAll(".animated")
+  for (let i = 0; i < reveals.length; i++) {
+    let windowHeight = window.innerHeight
+    let elementTop = reveals[i].getBoundingClientRect().top
+    let elementVisible = 50
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active")
+    } else {
+      reveals[i].classList.remove("active")
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal)
+
+// To check the scroll position on page load
+reveal()
