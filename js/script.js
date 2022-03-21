cff3cab74d200eef4c84b3ec521a28f9275d3675
spamhws <egorsanko@gmail.com>
2022-03-21 16:20:11 +0100

    comlete redesign of the Home page

--- js/script.js
+++ js/script.js
@@ -3,31 +3,25 @@
 const navList = document.getElementById("nav-lists-mobile")
 function Show() {
   navList.classList.add("_Menus-show")
-  console.log("Open")
 }
 
 function Hide() {
   navList.classList.remove("_Menus-show")
-  console.log("close")
 }
 
 // Image gallery buttons:
 
 const buttons = document.querySelectorAll("[data-carousel-button]")
 
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const offset = button.dataset.carouselButton === "next" ? 1 : -1
-    const slides = button
-      .closest("[data-carousel]")
-      .querySelector("[data-slides]")
+const slideLoop = () => {
+  const slides = document.querySelector("[data-slides]")
+  const activeSlide = slides.querySelector("[data-active]")
+  let newIndex = [...slides.children].indexOf(activeSlide) + 1
+  if (newIndex < 0) newIndex = slides.children.length - 1
+  if (newIndex >= slides.children.length) newIndex = 0
 
-    const activeSlide = slides.querySelector("[data-active]")
-    let newIndex = [...slides.children].indexOf(activeSlide) + offset
-    if (newIndex < 0) newIndex = slides.children.length - 1
-    if (newIndex >= slides.children.length) newIndex = 0
+  slides.children[newIndex].dataset.active = true
+  delete activeSlide.dataset.active
+}
 
-    slides.children[newIndex].dataset.active = true
-    delete activeSlide.dataset.active
-  })
-})
+setInterval(slideLoop, [5000])
