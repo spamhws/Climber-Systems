8572d5557060416e8d890e8c7bceb12c5ec1903e
spamhws <egorsanko@gmail.com>
2022-04-30 23:29:35 +0200

    JS bug fix n1

--- js/script.js
+++ js/script.js
@@ -11,18 +11,18 @@ function Hide() {
 
 // Image gallery buttons:
 
-// const slideLoop = () => {
-//   const slides = document.querySelector("[data-slides]")
-//   if (slides != null) {
-//     const activeSlide = slides.querySelector("[data-active]")
-//     let newIndex = [...slides.children].indexOf(activeSlide) + 1
-//     if (newIndex < 0) newIndex = slides.children.length - 1
-//     if (newIndex >= slides.children.length) newIndex = 0
+const slideLoop = () => {
+  const slides = document.querySelector("[data-slides]")
+  if (slides != null) {
+    const activeSlide = slides.querySelector("[data-active]")
+    let newIndex = [...slides.children].indexOf(activeSlide) + 1
+    if (newIndex < 0) newIndex = slides.children.length - 1
+    if (newIndex >= slides.children.length) newIndex = 0
 
-//     slides.children[newIndex].dataset.active = true
-//     delete activeSlide.dataset.active
-//   }
-// }
+    slides.children[newIndex].dataset.active = true
+    delete activeSlide.dataset.active
+  }
+}
 
 setInterval(slideLoop, [5000])
 
