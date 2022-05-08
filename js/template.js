a346b97dc42c6ddf5d2c8f7c53c68d4de6d6a528
spamhws <egorsanko@gmail.com>
2022-05-08 19:36:47 +0200

    all products added

--- js/template.js
+++ js/template.js
@@ -1,28 +1,25 @@
 const json = {
-  part1: "Message example: &#10;&#10;Hello, &#10;&#10;",
+  part1: 'Message example: &#10;&#10;Hello, &#10;&#10;',
   template: {
-    "Rice de-husking machine":
-      "I'd like to request more information about your Rice de-husking machine.",
+    'Rice de-husking machine': "I'd like to request more information about the Rice de-husking machine.",
+    'Welding Platform': "I'd like to request more information about the Welding Platform.",
+    'Modular Toilet': "I'd like to request more information about the Modular Toilet Superstructures.",
+    'Plastic Reactor': "I'd like to request more information about the 900 Degree Plastic to Diesel Reactor.",
   },
   part2:
-    "&#10;&#10;My phone number: &#10;My preferred method of contact is phone/email.&#10;&#10;Looking forward to hearing from you!",
-}
+    '&#10;&#10;My phone number: &#10;My preferred method of contact is phone/email.&#10;&#10;Looking forward to hearing from you!',
+};
 
-let lastViewedProduct = localStorage.getItem("lastViewedProduct")
+let lastViewedProduct = localStorage.getItem('lastViewedProduct');
 
-let textArea = document.querySelector("#form-message")
+let textArea = document.querySelector('#form-message');
 
 function templateConstructor(json) {
-  if (json["template"][lastViewedProduct] == undefined) return
+  if (json['template'][lastViewedProduct] == undefined) return;
 
-  let message =
-    json["part1"] + json["template"][lastViewedProduct] + json["part2"]
+  let message = json['part1'] + json['template'][lastViewedProduct] + json['part2'];
 
-  textArea.innerHTML = message
+  textArea.innerHTML = message;
 }
 
-templateConstructor(json)
-
-// fetch("../../js/template.json")
-//   .then((response) => response.json())
-//   .then((json) => templateConstructor(json))
+templateConstructor(json);
